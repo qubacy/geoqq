74a08acc5c9b4216bdec48b54694feb3556bf5cd
qubacy <souldzukaev@gmail.com>
2023-10-02 02:07:22 -0700

    Other: - Some minor changes including moving BottomSheet component-related classes to com.qubacy.geoqq.ui.common.component.bottomsheet;

---
+++ client/app/src/main/java/com/qubacy/geoqq/ui/common/component/bottomsheet/BottomSheetContentBehavior.kt
@@ -0,0 +1,12 @@
+package com.qubacy.geoqq.ui.common.component.bottomsheet
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.card.MaterialCardView
+
+class BottomSheetContentBehavior(context: Context, attributeSet: AttributeSet)
+    : BottomSheetBehavior<MaterialCardView>(context, attributeSet)
+{
+
+}
