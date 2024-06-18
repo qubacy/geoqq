768b87f6953a7d5161796ed9de40be48fac3ec4e
Leonid Artemev <leonidart14@gmail.com>
2024-06-18 14:47:38 +0700

    upd infra tests

--- server/own_services/geoqq_ws/internal/adapters/infrastructure/database/sql/postgre/database_test.go
+++ server/own_services/geoqq_ws/internal/adapters/infrastructure/database/sql/postgre/database_test.go
@@ -13,18 +13,23 @@ import (
 
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/network"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 const (
-	postgreDbName    = "domain"
-	postgreUsername  = "postgre"
-	postgrePassword  = "postgre"
-	pathToMigrations = "../../../../../../common/storage/domain/sql/postgre/migrations"
+	postgreDbName   = "domain"
+	postgreUsername = "postgre"
+	postgrePassword = "postgre"
 )
 
 var (
 	postgreHost        = ""
-	postgrePort uint16 = 0
+	postgrePort uint16 = 5432
+
+	postgreExternalHost        = ""
+	postgreExternalPort uint16 = 0
+	pathToMigrations           = "/common/storage/domain/sql/postgre/migrations" // will be changed!
 )
 
 var (
@@ -32,17 +37,38 @@ var (
 	occurrenceCount = 3
 )
 
-var postgresContainer *postgres.PostgresContainer
-var migrateContainer testcontainers.Container
+var (
+	postgresContainer *postgres.PostgresContainer = nil
+	migrateContainer  testcontainers.Container    = nil
+)
+
+// volume
+// -----------------------------------------------------------------------
+
+type ContainerMountSourceMigrations struct{}
+
+func (c ContainerMountSourceMigrations) Source() string {
+	return pathToMigrations
+}
+
+func (c ContainerMountSourceMigrations) Type() testcontainers.MountType {
+	return testcontainers.MountTypeBind
+}
 
 // -----------------------------------------------------------------------
 
 func setup() {
+	startPrefix := "\t\t\t"
 	ctx := context.Background()
-	var err error
+	var err error = nil
+
+	commonNet, err := network.New(ctx, network.WithDriver("bridge"))
+	if err != nil {
+		log.Fatalf("create common net err: %s", err)
+	}
 
 	// postgre
-	log.Println("start `postgre`")
+	log.Println(startPrefix + "start `postgre`")
 
 	{
 		postgresContainer, err = postgres.RunContainer(
@@ -52,20 +78,25 @@ func setup() {
 			postgres.WithPassword(postgrePassword),
 			postgres.WithDatabase(postgreDbName),
 
-			testcontainers.WithEnv(map[string]string{
-				"POSTGRES_HOST_AUTH_METHOD": "md5"}))
+			testcontainers.WithEnv(map[string]string{"POSTGRES_HOST_AUTH_METHOD": "md5"}),
+			network.WithNetwork([]string{}, commonNet),
 
-		// testcontainers.WithWaitStrategy(
-		// 	wait.ForLog("database system is ready to accept connections").
-		// 		WithOccurrence(occurrenceCount).
-		// 		WithStartupTimeout(startupTimeout)))
-
-		time.Sleep(5 * time.Second)
+			testcontainers.WithWaitStrategy(
+				wait.ForLog("listening").
+					WithOccurrence(occurrenceCount).
+					WithStartupTimeout(startupTimeout))) // ?
+		time.Sleep(250 * time.Millisecond)
 
 		var endpoint string
 		utl.RunFuncsRetErr(
 			func() error { return err },
 			func() error {
+				containerJson, err := postgresContainer.Inspect(ctx)
+				if err == nil {
+					postgreHost = containerJson.Config.Hostname
+				}
+				return err
+			}, func() error {
 				endpoint, err = postgresContainer.Endpoint(ctx, "")
 				return err
 			})
@@ -74,22 +105,37 @@ func setup() {
 		}
 
 		parts := strings.Split(endpoint, ":")
-		postgreHost = parts[0]
+		postgreExternalHost = parts[0]
 		postgrePort64, _ := strconv.ParseUint(parts[1], 10, 16)
-		postgrePort = uint16(postgrePort64)
+		postgreExternalPort = uint16(postgrePort64)
+
+		// ***
 
 		log.Printf("postgre host: %v\n", postgreHost)
-		log.Printf("postgre port: %v\n", postgrePort)
+		log.Printf("postgre db name: %v\n", postgreDbName)
+		log.Printf("postgre external host: %v\n", postgreExternalHost)
+		log.Printf("postgre external port: %v\n", postgreExternalPort)
 	}
 
 	// migrate
-	log.Println("start `migrate`")
+	log.Println(startPrefix + "start `migrate`")
 
 	{
+		wd, _ := os.Getwd()
+		wd = strings.Replace(wd, "\\", "/", -1)
+
+		ownServicesDir := "own_services"
+		index := strings.Index(wd, ownServicesDir)
+		wd = wd[:index+len(ownServicesDir)]
+		pathToMigrations = wd + pathToMigrations
+		log.Printf("path to migrations: %v", pathToMigrations)
+
+		// ***
+
 		reqContainer := testcontainers.ContainerRequest{
 			Image: "migrate/migrate",
 			Cmd: []string{
-				fmt.Sprintf("-path=%v", pathToMigrations),
+				fmt.Sprintf("-path=%v", "/migrations/"),
 				"-database",
 				fmt.Sprintf("postgres://%v:%v@"+
 					"%v:%v/%v?sslmode=disable",
@@ -98,6 +144,11 @@ func setup() {
 					postgreDbName),
 				"up",
 			},
+			Networks: []string{commonNet.Name}, // ?
+			Mounts: []testcontainers.ContainerMount{{
+				Source: ContainerMountSourceMigrations{},
+				Target: testcontainers.ContainerMountTarget("/migrations/"),
+			}},
 		}
 		migrateContainer, err = testcontainers.GenericContainer(
 			ctx,
@@ -106,11 +157,26 @@ func setup() {
 				Started:          true,
 			})
 		if err != nil {
-			log.Fatalf("migrate container err: %s", err)
+			log.Fatalf("migrate container err: %v", err)
+		}
+
+		for {
+			time.Sleep(250 * time.Millisecond)
+
+			containerJson, err := migrateContainer.Inspect(ctx)
+			if err != nil {
+				log.Fatalf("migrate container inspect with err: %v", err)
+			}
+
+			if containerJson.State.Status == "exited" {
+				break
+			}
 		}
 	}
 
 	// inflate
+
+	log.Println("setup [OK]")
 }
 
 func teardown() {
@@ -144,8 +210,8 @@ func Test_InsertMateMessage(t *testing.T) {
 	db, err := New(context.Background(), Dependencies{
 		Username:     postgreUsername,
 		Password:     postgrePassword,
-		Host:         postgreHost,
-		Port:         postgrePort,
+		Host:         postgreExternalHost,
+		Port:         postgreExternalPort,
 		DatabaseName: postgreDbName,
 	})
 	if err != nil {
